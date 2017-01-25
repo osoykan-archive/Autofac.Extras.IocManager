567074eb4b1c5f938a2e0a0ac9c28a0dfe9d783a
osoykan <oguzhansoykan@gmail.com>
2017-01-26 00:56:49 +0300

    RegistrationCompleted event added

---
+++ src/Autofac.Extras.IocManager/RegistrationCompletedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Autofac.Extras.IocManager
+{
+    public class RegistrationCompletedEventArgs : EventArgs
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RegistrationCompletedEventArgs" /> class.
+        /// </summary>
+        /// <param name="resolver">The resolver.</param>
+        public RegistrationCompletedEventArgs(IResolver resolver)
+        {
+            Resolver = resolver;
+        }
+
+        /// <summary>
+        ///     Gets or sets the resolver.
+        /// </summary>
+        /// <value>
+        ///     The resolver.
+        /// </value>
+        public IResolver Resolver { get; set; }
+    }
+}
---
+++ test/Autofac.Extras.IocManager.Tests/RegistrationCompletedEventTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Autofac.Extras.IocManager.TestBase;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Autofac.Extras.IocManager.Tests
+{
+    public class RegistrationCompletedEventTests : TestBaseWithIocBuilder
+    {
+        public RegistrationCompletedEventTests()
+        {
+            SampleClass sampleClassInstance = null;
+
+            Building(builder =>
+            {
+                builder.RegisterServices(r => r.RegisterType<SampleClass>());
+
+                builder.RegisterServices(r => r.RegistrationCompleted += (sender, args) =>
+                {
+                    var scopeResolver = args.Resolver.Resolve<IScopeResolver>();
+
+                    using (IScopeResolver scope = scopeResolver.BeginScope())
+                    {
+                        sampleClassInstance = scope.Resolve<SampleClass>();
+                    }
+                });
+            });
+
+            sampleClassInstance.DisposeCount.ShouldBe(1);
+        }
+
+        [Fact]
+        public void RegistrationCompletedEvent_Should_Work()
+        {
+        }
+
+        public class SampleClass : IDisposable
+        {
+            public int DisposeCount { get; set; }
+
+            public void Dispose()
+            {
+                DisposeCount++;
+            }
+        }
+    }
+}
